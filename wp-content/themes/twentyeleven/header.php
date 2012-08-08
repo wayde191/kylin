f3a36493d58eadfcacddd5afd143259ce408128c
wayde191 <wsun191@gmail.com>
2012-08-08 23:56:22 +0800

    add ico file to theme

--- wp-content/themes/twentyeleven/header.php
+++ wp-content/themes/twentyeleven/header.php
@@ -46,6 +46,7 @@
 
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/ico/16.ico"></link>
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
